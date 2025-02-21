0dfb97da32fa9dbf4eb9f06565560bcf1c8cc915
ItsFeold <feoldsmedia@gmail.com>
2025-02-21 15:19:08 +0200

    Perf: Improved eval function

--- src/engine.c
+++ src/engine.c
@@ -17,25 +17,15 @@ int choose_move(char *fen, char *moves, int timeout) {
     init_LUT();
     init_tables();
     init_zobrist();
-<<<<<<< HEAD
-    init_transposition_table(1024 / sizeof(TTentry));
-=======
     init_transposition_table(1024 * 1024 * 1024 / sizeof(TTentry));
->>>>>>> e2b7c50d0d1c8d4a4fd8aed3854035330817802d
 
     Board board;
     board.key = 0ULL;
     loadFEN(&board, fen);
 
     MoveList list = first_list(moves, &board);
-<<<<<<< HEAD
-    Move best = iterative_deepening_search(&board, 6, list);
-    // board.key = update_board_key(&board);
-    // printf("U64 key of board is: %" PRIu64 "\n", board.key);
-=======
     Move best = iterative_deepening_search(&board, 8, list);
     print_move(&best);
->>>>>>> e2b7c50d0d1c8d4a4fd8aed3854035330817802d
     for (int i = 0; i < list.count; i++) {
         if (move_equals(&best, &list.moves[i])) {
             return i;
