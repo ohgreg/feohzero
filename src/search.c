a385169cfa761b52aa9ed49a43f5a01d3efd1006
ItsFeold <feoldsmedia@gmail.com>
2025-02-23 11:05:03 +0200

    Fix: Fixes stalemating on checkmates

--- src/search.c
+++ src/search.c
@@ -84,7 +84,8 @@ int depth_limited_search(Board *board, int depth, int is_root, Move *best_move, 
     qsort(list.moves, list.count, sizeof(Move), sort_moves);
 
     // this is draw or mate. Cooked if stalemate.
-    if (list.count == 0) return (board->turn ? INF : -INF);
+    if (list.count == 0 && (generate_opponent_attacks(board) & board->pieces[board->turn][KING]) != 0) return (board->turn ? INF : -INF);
+    else if (list.count == 0 && (generate_opponent_attacks(board) & board->pieces[board->turn][KING]) == 0) return 0;
 
     int best_current_score = (side == WHITE) ? -INF : INF;
     Move best_current_move = list.moves[0];
